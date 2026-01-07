8ab04a8f8cc003e083d9e599677fce07c5600a2b
JuanJesusRamirez <jramirez@flar.net>
2026-01-07 18:11:25 -0500

    refactor: update resource names and configurations in main.tf; add troubleshooting document for federated credentials

--- infra/main.tf
+++ infra/main.tf
@@ -3,22 +3,19 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = var.sku_tier
-    size = var.sku_size
-  }
+  os_type             = "Windows"
+  sku_name            = var.sku_size
 }
 
 resource "azurerm_app_service" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_plan_id = azurerm_service_plan.asp.id
 
 
   app_settings = {

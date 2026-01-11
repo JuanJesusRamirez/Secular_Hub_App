83bcc3801d41c03610a6a008a902e0dab702b13a
JuanJesusRamirez <jramirez@flar.net>
2026-01-11 08:09:37 -0500

    feat: update Terraform backend state file to version 7

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,7 +6,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "rg-secular-hub-tfstate"
     container_name      = "tfstate"
-    key                 = "secular-hub/infra-v6.tfstate"
+    key                 = "secular-hub/infra-v7.tfstate"
     # supply `storage_account_name` via `terraform init -backend-config="storage_account_name=<name>"`
   }
 } 

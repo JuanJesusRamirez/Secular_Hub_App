36cd800731a0547d02b96e26a439178e4f7b0783
JuanJesusRamirez <jramirez@flar.net>
2026-01-11 08:23:16 -0500

    refactor: remove unused variables from Terraform configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,6 @@
-variable "env" {
-  type        = string
-  description = "Environment name (dev)"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure location"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Resource Group for DEV resources"
-}
-
-variable "resource_group_suffix" {
-  type        = string
-  description = "Optional suffix appended to the resource group name to create isolated deployments (e.g. 'fresh')"
-  default     = ""
-}
-
-variable "acr_name" {
-  type        = string
-  description = "Name for the Azure Container Registry (must be globally unique)"
-}
-
-variable "container_app_name" {
-  type        = string
-  description = "Container App name"
-}
-
 variable "image_tag" {
   type        = string
   description = "Immutable image tag to deploy (commit SHA)"
+  default     = "latest"
 }
 

d4a21a5e0701c2dfceee62445063156d91c9c4f4
JuanJesusRamirez <jramirez@flar.net>
2026-01-21 11:38:44 -0500

    Fix bug on 2026 outlook related to Outlook Thesis -> Calls

--- components/manus/ManusReport.tsx
+++ components/manus/ManusReport.tsx
@@ -73,7 +73,7 @@ export default function ManusReport() {
         },
         {
             number: String(totalCalls),
-            label: 'Outlook Thesis',
+            label: 'CALLS',
             description: 'Investment Outlooks'
         },
         {
